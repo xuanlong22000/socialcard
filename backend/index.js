fadcf88f57dfb4fd78cef422c5782167cc6f0136
Long Bui <xuanlong22000@gmail.com>
2023-03-13 16:07:10 +0700

    URL Mongo Atlas

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,9 @@ const app = express()
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
-
-const CONNECTION_URL = 'mongodb://0.0.0.0:27017/socialCard'
+//mongodb+srv://xuanlong22000:Xuanlong640@cluster0.m584mzj.mongodb.net/?retryWrites=true&w=majority
+//mongodb://0.0.0.0:27017/socialCard
+const CONNECTION_URL = 'mongodb+srv://xuanlong22000:Xuanlong640@cluster0.m584mzj.mongodb.net/?retryWrites=true&w=majority'
 
 const PORT = process.env.PORT || 5000
 
