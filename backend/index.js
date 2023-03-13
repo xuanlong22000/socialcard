5cc253d9f51448581470819bb5ce01b4a43a8498
Long Bui <xuanlong22000@gmail.com>
2023-03-13 11:12:22 +0700

    fix error node v18

--- backend/index.js
+++ backend/index.js
@@ -15,14 +15,14 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
-const CONNECTION_URL = 'mongodb://localhost:27017/socialCard'
+const CONNECTION_URL = 'mongodb://0.0.0.0:27017/socialCard'
 
 const PORT = process.env.PORT || 5000
 
 
 app.use('/posts', postsRoute)
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) }))
     .catch((error) => console.log(error))
 
